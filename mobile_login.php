983555e4107894a72c37fa25220bac73f4cb84d6
Khairol Anwar <whyuwanttoknowm@gmail.com>
2024-06-23 02:08:12 +0800

    sync

--- mobile_login.php
+++ mobile_login.php
@@ -1,27 +1,26 @@
 <?php
+$hostname = "localhost:3307";
+$username = "root";
+$password = "";
+$dbname = "polyglot";
 
-    $hostname = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "student";
-            
-    $connect = mysqli_connect($hostname, $username, $password, $dbname) 
-    OR DIE ("Connection Failed");
-            
-    $username = $_GET["username"];
-    $pass = $_GET["password"];
-            
-    $sql = "SELECT * FROM login WHERE username = '$username' AND password = '$pass' ";
-            
-    $sendsql = mysqli_query($connect, $sql);
-            
-    if($sendsql){
-        if(mysqli_num_rows($sendsql) > 0)
-            {
-               echo "successfully login";
-            }
-            else{
-                   echo "Query Failed";
-            }
+$connect = mysqli_connect($hostname, $username, $password, $dbname) 
+OR DIE ("Connection Failed");
+
+$username = $_POST["username"];
+$pass = $_POST["password"];
+
+$sql = "SELECT * FROM login WHERE username = '$username' AND password = '$pass' ";
+
+$sendsql = mysqli_query($connect, $sql);
+
+if($sendsql){
+    if(mysqli_num_rows($sendsql) > 0)
+    {
+        echo "Successfully login!";
     }
+    else{
+        echo "Query Failed";
+    }
+}
 ?>
--- mobile_register.php
+++ mobile_register.php
@@ -1,37 +1,40 @@
 <?php
- $hostname = "localhost";
- $username = "root";
- $password = "";
- $dbname = "student";
+    $hostname = "localhost:3307";
+    $username = "root";
+    $password = "";
+    $dbname = "polyglot";
 
- $connect = mysqli_connect($hostname, $username, $password, $dbname) 
- OR DIE ("Connection Failed");
+    $connect = mysqli_connect($hostname, $username, $password, $dbname) 
+    OR DIE ("Connection Failed");
 
- $username = $_GET["username"];
- $pass = $_GET["password"];
+    $username = $_GET["Username"];
+    $pass = $_GET["Password"];
 
- $sql = "SELECT * FROM login WHERE username = '$username' AND password = '$pass' ";
+    $sql = "SELECT * FROM login WHERE username = '$username' AND password = '$pass' ";
+ 
+    $sendsql = mysqli_query($connect, $sql);
 
- $sendsql = mysqli_query($connect, $sql);
+    if($sendsql){
+        if(mysqli_num_rows($sendsql) > 0)
+        {
+            echo "<h1>User already exist</h1>";
+        }
+        else{
+            $sql2 = "INSERT INTO login(username, password) VALUES ('$username' , '$pass')";
 
- if($sendsql){
-     if(mysqli_num_rows($sendsql) > 0)
-     {
-         echo "<h1>User already exist</h1>";
-     }
-     else{
-         $sql2 = "INSERT INTO login(username, password) VALUES ('$username' , '$pass')";
+            $sendsql2 = mysqli_query($connect, $sql2);
 
-         $sendsql2 = mysqli_query($connect, $sql2);
+            if($sendsql2){
+                echo "Successfully Register!";
+            }
+            else{
+                echo "Query Failed";
+            }
+        }
+    }else{
+       echo "QUERY FAILED!";
+    }
 
-         if($sendsql2){
-             echo "successfully register";
-         }
-         else{
-             echo "Query Failed";
-         }
-     }
- }else{
-    echo "QUERY FAILED!";
- }
+    
+
 ?>
